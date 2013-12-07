3edbfbf4b01fabee84777983cecf8a59c9afad42
chris crowd <zent29@gmail.com>
2013-12-06 22:06:32 -0500

    start project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MuscleflowApp::Application.config.secret_key_base = '8f6ee822483833c09d180cd9a814cf63a45cdeb4c004663288917682051361e33e8d03b1e3a5e9bfb27e9afbd5faf6b0f1b5ec2e477b6527c4937cdd4957d067'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MuscleflowApp::Application.config.session_store :cookie_store, key: '_muscleflow_app_session'
